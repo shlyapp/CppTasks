b59b57c3adecb9fa8ff4adec4ddc614bd5cf31f3
shlyapp <shlyapp.exe@gmail.com>
2022-10-10 23:13:41 +0400

    Исправление 2-1-1

--- 2-1-1/2-1-1.cpp
+++ 2-1-1/2-1-1.cpp
@@ -12,11 +12,11 @@ int main()
 
     // инициализруем и считываем число a
     double a = 0;
-    scanf("%f", &a);
+    scanf("%lf", &a);
 
     // рассчитываем числа y и z, согласно таблице 1.1
-    double y = 2 * pow(sin(3 * PI - 2 * a), 2) * pow(cos(5 * PI + 2 * a), 2);
-    double z = 1 / 4 - 1 / 4 * sin((5 * PI) / 2 - 8 * a);
+    double y = 2 * pow(sin((3 * PI) - (2 * a)), 2) * pow(cos((5 * PI) + (2 * a)), 2);
+    double z = 0.25 - (0.25 * sin((5 * PI) / 2 - 8 * a));
 
     printf("Значение y: %f\n", y);
     printf("Значение z: %f\n", z);
